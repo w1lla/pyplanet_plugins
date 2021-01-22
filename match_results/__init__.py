7ef4b2caad663e357c6731514173f8ac9e957e70
W1lla van den munckhof <w1llaopgezwolle@gmail.com>
2021-01-22 18:36:12 +0100

    Proper update
    See matchresults.html as example

--- match_results/__init__.py
+++ match_results/__init__.py
@@ -1,6 +1,8 @@
 
 from pyplanet.apps.config import AppConfig
 from pyplanet.apps.core.trackmania import callbacks as tm_signals
+from pyplanet.contrib.setting import Setting
+from pyplanet.utils import times
 from .models import Save
 
 class match_results(AppConfig):
@@ -17,12 +19,21 @@ class match_results(AppConfig):
 		Initializes the plugin.
 		"""
 		super().__init__(*args, **kwargs)
+		self.setting_html = Setting(
+			'html', 'Use HTML or DB as output', Setting.CAT_BEHAVIOUR, type=bool,
+			description='Where HTML is true, DB is False',
+			default=False
+		)
 
 	async def on_start(self):
 		"""
 		Called on starting the application.
 		"""
-				# Listen to signals.
+		
+		# Init settings.
+		await self.context.setting.register(self.setting_html)
+		
+		# Listen to signals.
 		self.context.signals.listen(tm_signals.scores, self.scores)
 		
 	async def scores(self, section, players, **kwargs):
@@ -30,15 +41,40 @@ class match_results(AppConfig):
 			await self.handle_scores(players)
 			
 	async def handle_scores(self, players):
+		rank = 1
 		for player in players:
-			#print(player['player'])
+			print(player['player'].nickname)
 			mappoints = int(player['map_points'])
-			position_endmap = int(player['rank'])
+			nickname = player['player'].nickname
+			increment_rank = rank
+			position_endmap = int(increment_rank)
 			best_racetime = int(player['best_race_time'])
 			#print(mappoints)
 			#print(position_endmap)
 			#print(best_racetime)
-			end_map = Save(map=self.instance.map_manager.current_map, player=player['player'], map_points=mappoints, rank=position_endmap, bestracetime=best_racetime)
-			#print(end_map)
-			await end_map.save()
-		
+			if await self.setting_html.get_value():
+				end_map = Save(map=self.instance.map_manager.current_map, player=player['player'], map_points=mappoints, rank=position_endmap, bestracetime=best_racetime)
+				#print(end_map)
+				await end_map.save()
+			else:
+				with open('matchresults.html', 'a') as myFile:
+					myFile.write('<html>')
+					myFile.write('<body>')
+					myFile.write('MapName: {} &nbsp; &nbsp; &nbsp;'.format(self.instance.map_manager.current_map.name));
+					myFile.write('<table width=\"100%\" border=\"0\" align=\"center\">');
+					myFile.write('<tr>');
+					myFile.write('<td width=\"60\" class=\"tablehead\" bgcolor="#FFFFF">Rank</td>');
+					myFile.write('<td width=\"150\" class=\"tablehead\" bgcolor="#FFFFF">Nickname</td>');
+					myFile.write('<td width=\"150\" class=\"tablehead\" bgcolor="#FFFFF">Best Race Time</td>');
+					myFile.write('<td width=\"80\" class=\"tablehead\" bgcolor="#FFFFF">Points</td>');
+					myFile.write('</td>');
+					myFile.write('<tr>');
+					myFile.write('<td class=\"celltext\" bgcolor=\"#FFFFF\">{}</td>'.format(position_endmap));
+					myFile.write('<td class=\"celltext\" bgcolor=\"#FFFFF\">{}</td>'.format(nickname));
+					myFile.write('<td class=\"celltext\" bgcolor=\"#FFFFF\">{}</td>'.format(times.format_time(int(best_racetime))));
+					myFile.write('<td class=\"celltext\" bgcolor=\"#FFFFF\">{}</td>'.format(mappoints));
+					myFile.write('</tr>');
+					myFile.write('</table>');
+					myFile.write('</body>');
+					myFile.write('</html>');
+					rank += 1
